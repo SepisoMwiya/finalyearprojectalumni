df8d6e51bf58f2dcccc28ebf98ffed27946dd599
SepisoMwiya <sepisomwiya20@gmail.com>
2024-11-23 15:31:56 +0200

    tabs with icons

--- app/components/admin/content-management-tabs.tsx
+++ app/components/admin/content-management-tabs.tsx
@@ -1,17 +1,30 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import ProjectManagement from "./project-management";
 import CareerManagement from "./career-management";
+import { Gift, Newspaper, Projector, User } from "lucide-react";
 
 export default function ContentManagementTabs() {
   return (
     <Tabs defaultValue="projects" className="space-y-4">
       <TabsList>
-        <TabsTrigger value="projects">Projects</TabsTrigger>
-        <TabsTrigger value="careers">Careers</TabsTrigger>
-        <TabsTrigger value="news" disabled>
+        <TabsTrigger value="projects" className="flex items-center gap-2">
+          <Projector />
+          Projects
+        </TabsTrigger>
+        <TabsTrigger value="careers" className="flex items-center gap-2">
+          <User />
+          Careers
+        </TabsTrigger>
+        <TabsTrigger value="news" className="flex items-center gap-2" disabled>
+          <Newspaper />
           News
         </TabsTrigger>
-        <TabsTrigger value="opportunities" disabled>
+        <TabsTrigger
+          value="opportunities"
+          className="flex items-center gap-2"
+          disabled
+        >
+          <Gift />
           Opportunities
         </TabsTrigger>
       </TabsList>
